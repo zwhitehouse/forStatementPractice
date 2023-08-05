28ce457a65d3bd8dc04d189bf400d48a4591ed3d
Zack Whitehouse <zackwhitehouse225@gmail.com>
2023-08-05 18:44:49 -0400

    add for statement

---
+++ friends.js
@@ -0,0 +1,17 @@
+// var numbers = [1,2,3,4,31,32,33,34,61];
+
+// for (var i in numbers) {
+//     if (numbers[i] > 30) {
+//         console.log(numbers[i]); 
+//     }
+// }
+
+var numbs = [59,60,61,62,63,64,65,66,67]
+
+for (var i in numbs) {
+    if (numbs[i] < 60) {
+        console.log(numbs[i]); 
+    }
+}
+
+
